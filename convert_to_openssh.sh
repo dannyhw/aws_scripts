96b122e679db49b891871e5cdafcede79f8cc26b
Danny <danny.h.williams@hotmail.com>
2016-12-08 15:42:46 +0000

    match only pem files

--- convert_to_openssh.sh
+++ convert_to_openssh.sh
@@ -2,10 +2,10 @@ for file in *.pem; do
     ssh-keygen -i -f "$file" > "open_$file"
 done
 
-for file in open_*;do
+for file in open_*.pem;do
     name="${file%%.pem}"
     name="${name##open_}"
     sed -i -- "s/==/== $name/g" $file
 done
-cat open_* > opensshkeys
-rm open_*
+cat open_*.pem > opensshkeys
+rm open_*.pem
